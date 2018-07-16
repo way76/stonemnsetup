e66733381aad7c0cd798f80cfb5fb4ef34a673af
cryptotym <38680812+cryptotym@users.noreply.github.com>
2018-07-16 16:49:33 -0700

    Updated menu

--- stonemnsetup.sh
+++ stonemnsetup.sh
@@ -337,7 +337,7 @@ function newInstall() {
        read -p "Are you sure? (y/n): " yn </dev/tty
        case $yn in
            [Yy]* ) echo "This may take some time, be patient and wait for the prompts."; sleep 2; installNode;;
-           [Nn]* ) echo "Exiting..."; sleep 2; break;;
+           [Nn]* ) echo "Restarting..."; sleep 2; clear; mainMenu;;
            * ) echo "Please answer yes or no.";;
        esac
    done
@@ -349,22 +349,62 @@ function upgradeOnly() {
        read -p "Are you sure? (y/n): " yn </dev/tty
        case $yn in
            [Yy]* ) echo "This should only take a moment."; sleep 2; upgradeNode;;
-           [Nn]* ) echo "Restarting..."; sleep 2; break; chooseInstall;;
+           [Nn]* ) echo "Restarting..."; sleep 2; clear; mainMenu;;
            * ) echo "Please answer yes or no.";;
        esac
    done
  }
 
-function chooseInstall() {
+ function mainMenu () {
 
-   title="Welcome To The STONE Masternode setup"
-
-   echo "Warming up..."
-   clear
-   echo "$title"
-   newInstall
-   
+       E='echo -e';e='echo -en';trap "R;exit" 2
+     ESC=$( $e "\e")
+    TPUT(){ $e "\e[${1};${2}H";}
+   CLEAR(){ $e "\ec";}
+   CIVIS(){ $e "\e[?25l";}
+    DRAW(){ $e "\e%@\e(0";}
+   WRITE(){ $e "\e(B";}
+    MARK(){ $e "\e[7m";}
+  UNMARK(){ $e "\e[27m";}
+       R(){ CLEAR ;stty sane;$e "\ec\e[37;44m\e[J";};
+    HEAD(){ DRAW
+            for each in $(seq 1 13);do
+            $E "   x                                          x"
+            done
+            WRITE;MARK;TPUT 1 5
+            $E "         STONE MASTERNODE SETUP    ";UNMARK;}
+            i=0; CLEAR; CIVIS;NULL=/dev/null
+    FOOT(){ MARK;TPUT 13 5
+            printf "          ENTER - SELECT,NEXT            ";UNMARK;}
+   ARROW(){ read -s -n3 key 2>/dev/null >&2
+            if [[ $key = $ESC[A ]];then echo up;fi
+            if [[ $key = $ESC[B ]];then echo dn;fi;}
+      M0(){ TPUT  4 20;$e "New Install";}
+      M1(){ TPUT  5 20;$e "Upgrade";}
+      M2(){ TPUT  6 20;$e "EXIT   ";}
+      M3(){ TPUT  7 20;$e "";}
+       LM=3
+    MENU(){ for each in $(seq 0 $LM);do M${each};done;}
+     POS(){ if [[ $cur == up ]];then ((i--));fi
+            if [[ $cur == dn ]];then ((i++));fi
+            if [[ $i -lt 0   ]];then i=$LM;fi
+            if [[ $i -gt $LM ]];then i=0;fi;}
+ REFRESH(){ after=$((i+1)); before=$((i-1))
+            if [[ $before -lt 0  ]];then before=$LM;fi
+            if [[ $after -gt $LM ]];then after=0;fi
+            if [[ $j -lt $i      ]];then UNMARK;M$before;else UNMARK;M$after;fi
+            if [[ $after -eq 0 ]] || [ $before -eq $LM ];then
+            UNMARK; M$before; M$after;fi;j=$i;UNMARK;M$before;M$after;}
+    INIT(){ R;HEAD;FOOT;MENU;}
+      SC(){ REFRESH;MARK;$S;$b;cur=`ARROW`;}
+      ES(){ MARK;$e "ENTER = main menu ";$b;read;INIT;};INIT
+   while [[ "$O" != " " ]]; do case $i in
+         0) S=M0;SC;if [[ $cur == "" ]];then  R; newInstall;fi;;
+         1) S=M1;SC;if [[ $cur == "" ]];then  R; upgradeOnly; fi;;
+         3) S=M2;SC;if [[ $cur == "" ]];then R;exit 0;fi;;
+  esac;POS;done
  }
+
 
 function upgradeNode() {
   purgeOldInstallation
@@ -389,4 +429,4 @@ function installNode() {
 
 ##### Main #####
 
-chooseInstall
+mainMenu
