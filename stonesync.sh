f69fb059344465b005ad53ecb49ff85a371e47c9
way76 <43696467+way76@users.noreply.github.com>
2019-03-12 15:33:14 +0100

    Update stonesync.sh

--- stonesync.sh
+++ stonesync.sh
@@ -11,18 +11,18 @@ MNBLOCK=$(cd /usr/local/bin &&./stone-cli getblockcount)
 BOOTSTRAPURL='https://github.com/stonecoinproject/Stonecoin/releases/download/Bootstrapv2.0/stonecore.tar.gz'
 
 start(){
-echo "$(date +%F_%T) **Initializing STONE Sync Manager**" >> ~/.stonesyncmanager/stonesync.log
+echo "$(date +%F_%T) **Initializing STONE Sync Manager**" >> stonesync.log
 sleep $[ ( $RANDOM % 60 )  + 1 ]s
 isMnRunning
 }
 
 isMnRunning(){
-echo "$(date +%F_%T) Checking STONE service.." >> ~/.stonesyncmanager/stonesync.log
+echo "$(date +%F_%T) Checking STONE service.." >> stonesync.log
 #sleep 3
 MNACTIVE=$(systemctl is-active Stone.service)
 sleep 2
 if [ $MNACTIVE = "active" ]; then
-  echo "$(date +%F_%T) STONE service active!" >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) STONE service active!" >> stonesync.log
   #printBlock
   sleep 2
   checkServer
@@ -32,49 +32,49 @@ fi
 }
 
 checkServer(){
-echo "$(date +%F_%T) Pinging explorer.." >> ~/.stonesyncmanager/stonesync.log
+echo "$(date +%F_%T) Pinging explorer.." >> stonesync.log
 # EXPBLOCK=$(curl -s4 "http://explorer.stonecoin.rocks/api/getblockcount")
 if [ "$EXPBLOCK" -eq "$EXPBLOCK" ];
 then
-  echo "$(date +%F_%T) Successful ping!" >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Successful ping!" >> stonesync.log
   isDaemonReporting
 else
-  echo "$(date +%F_%T) **ERROR** STONE Explorer down, try again later!" >> ~/.stonesyncmanager/stonesync.log
-  echo "$(date +%F_%T) Exiting!" >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) **ERROR** STONE Explorer down, try again later!" >> stonesync.log
+  echo "$(date +%F_%T) Exiting!" >> stonesync.log
   endLog
 fi
 }
 
 printBlock(){
 #MNBLOCK=$(cd /usr/local/bin &&./stone-cli getblockcount)
-  echo -e "$(date +%F_%T) $MNBLOCK" >> ~/.stonesyncmanager/stonesync.log
+  echo -e "$(date +%F_%T) $MNBLOCK" >> stonesync.log
 }
 
 isDaemonReporting(){
 #MNBLOCK=$(cd /usr/local/bin &&./stone-cli getblockcount)
 #sleep 3
-echo "$(date +%F_%T) Verfying Daemon.." >> ~/.stonesyncmanager/stonesync.log
+echo "$(date +%F_%T) Verfying Daemon.." >> stonesync.log
 if [ -z "$MNBLOCK" ]
 then
-  echo "$(date +%F_%T) Daemon not reporting data, if this persists a resinstall may be required!" >> ~/.stonesyncmanager/stonesync.log
-  echo "$(date +%F_%T) Exiting!" >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Daemon not reporting data, if this persists a resinstall may be required!" >> stonesync.log
+  echo "$(date +%F_%T) Exiting!" >> stonesync.log
   endLog
 else
-echo "$(date +%F_%T) Daemon functional!" >> ~/.stonesyncmanager/stonesync.log
+echo "$(date +%F_%T) Daemon functional!" >> stonesync.log
 checkBlock
 fi
 }
 checkBlock(){
 #MNBLOCK=$(cd /usr/local/bin &&./stone-cli getblockcount)
 #sleep 2
-echo "$(date +%F_%T) Verifying block height.." >> ~/.stonesyncmanager/stonesync.log
-echo "$(date +%F_%T) Masternode Block $MNBLOCK.." >> ~/.stonesyncmanager/stonesync.log
-echo "$(date +%F_%T) Explorer Block $EXPBLOCK.." >> ~/.stonesyncmanager/stonesync.log
+echo "$(date +%F_%T) Verifying block height.." >> stonesync.log
+echo "$(date +%F_%T) Masternode Block $MNBLOCK.." >> stonesync.log
+echo "$(date +%F_%T) Explorer Block $EXPBLOCK.." >> stonesync.log
 if [ "$MNBLOCK" -ge "$EXPBLOCKLOW" ] && [ "$MNBLOCK" -le "$EXPBLOCKHIGH" ]; then
-  echo "$(date +%F_%T) Block height matches!" >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Block height matches!" >> stonesync.log
   complete
 else
-  echo "$(date +%F_%T) Block mismatch, double checking.." >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Block mismatch, double checking.." >> stonesync.log
   doubleCheckBlock
 fi
 }
@@ -85,21 +85,21 @@ MNBLOCK=$(cd /usr/local/bin &&./stone-cli getblockcount)
 sleep 2
 EXPBLOCK=$(curl -s4 "http://explorer.stonecoin.rocks/api/getblockcount")
 sleep 2
-echo "$(date +%F_%T) Masternode Block $MNBLOCK" >> ~/.stonesyncmanager/stonesync.log
-echo "$(date +%F_%T) Explorer Block $EXPBLOCK" >> ~/.stonesyncmanager/stonesync.log
+echo "$(date +%F_%T) Masternode Block $MNBLOCK" >> stonesync.log
+echo "$(date +%F_%T) Explorer Block $EXPBLOCK" >> stonesync.log
 EXPBLOCKLOW=$(expr $EXPBLOCK - 4)
 EXPBLOCKHIGH=$(expr $EXPBLOCK + 4)
 if [ "$MNBLOCK" -ge "$EXPBLOCKLOW" ] && [ "$MNBLOCK" -le "$EXPBLOCKHIGH" ]; then
-  echo "$(date +%F_%T) Block Height matches!" >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Block Height matches!" >> stonesync.log
   complete
 else
-  echo "$(date +%F_%T) Confirmed out of sync, running resync function.." >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Confirmed out of sync, running resync function.." >> stonesync.log
   reSync
 fi
 }
 
 reEnableSystemd() {
-  echo "$(date +%F_%T) STONE daemon not running, attempting to re-enable.." >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) STONE daemon not running, attempting to re-enable.." >> stonesync.log
   sleep 1
   systemctl daemon-reload
   sleep 3
@@ -109,25 +109,25 @@ reEnableSystemd() {
   MNACTIVE=$(systemctl is-active Stone.service)
   sleep 2
   if [ $MNACTIVE = "active" ]; then
-    echo "$(date +%F_%T) STONE service active!" >> ~/.stonesyncmanager/stonesync.log
-    echo "$(date +%F_%T) Verifying block height.." >> ~/.stonesyncmanager/stonesync.log
+    echo "$(date +%F_%T) STONE service active!" >> stonesync.log
+    echo "$(date +%F_%T) Verifying block height.." >> stonesync.log
     isMnRunning
     #printBlock
   else
-    echo "$(date +%F_%T) ERROR Unable to start STONE service, Please re-install using the official script!" >> ~/.stonesyncmanager/stonesync.log
-    echo "$(date +%F_%T) STONE masternode tutorial can be found here: https://github.com/stonecoinproject/stonemnsetup" >> ~/.stonesyncmanager/stonesync.log
-    echo "$(date +%F_%T) Exiting!" >> ~/.stonesyncmanager/stonesync.log
+    echo "$(date +%F_%T) ERROR Unable to start STONE service, Please re-install using the official script!" >> stonesync.log
+    echo "$(date +%F_%T) STONE masternode tutorial can be found here: https://github.com/stonecoinproject/stonemnsetup" >> stonesync.log
+    echo "$(date +%F_%T) Exiting!" >> stonesync.log
     endLog
   fi
 }
 
 endLog(){
-echo "$(date +%F_%T) -------------------------------End Log-------------------------------" >> ~/.stonesyncmanager/stonesync.log
+echo "$(date +%F_%T) -------------------------------End Log-------------------------------" >> stonesync.log
 exit
 }
 
 function reSync() {
-  echo "$(date +%F_%T) Disabling Stone.service.." >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Disabling Stone.service.." >> stonesync.log
   stone-cli invalidateblock 0000000005877dd963455c138f7562acf67ce5985ca363a84c93e40f8fe57e1c
   systemctl restart Stone.service
   sleep 10
@@ -152,23 +152,23 @@ function reSync() {
 }
 
 function addBootstrap() {
-  echo "$(date +%F_%T) Downloading bootstrap.." >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Downloading bootstrap.." >> stonesync.log
   cd ~/
   wget -q $BOOTSTRAPURL
   tar -xzf stonecore.tar.gz
   rm stonecore.tar.gz
-  echo "$(date +%F_%T) Bootstrap implemented successfully!" >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Bootstrap implemented successfully!" >> stonesync.log
   sleep 1
 }
 
 startMasternode(){
-  echo "$(date +%F_%T) Starting masternode.." >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) Starting masternode.." >> stonesync.log
   $(stone-cli masternode start)
   complete
 }
 
 complete(){
-  echo "$(date +%F_%T) STONE Sync Manager Complete!" >> ~/.stonesyncmanager/stonesync.log
+  echo "$(date +%F_%T) STONE Sync Manager Complete!" >> stonesync.log
   endLog
 }
 
